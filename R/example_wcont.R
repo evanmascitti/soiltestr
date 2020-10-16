968b8a93dfd306bfa571cddd91801018d28582d4
‘evanmascitti’ <evanmascitti@gmail.com>
2020-10-16 00:06:00 -0400

    fixed a couple typos in man file for example_wcont data

--- R/example_wcont.R
+++ R/example_wcont.R
@@ -1,12 +1,18 @@
-#' The observations include the tin with the wet sample, the tin with the dry
-#' sample, and the tin tare. In normal circumstances the tin tare would be
-#' looked up using a `left_join` and another data frame of tin numbers and their
-#' known masses.
+#' Typical soil water contents near the plastic limit
+#'
+#' The observations include the tin with the wet sample, the tin with the
+#' oven-dry sample, and the tin tare. In typical usage the tin tare would be
+#' looked up using a `left_join` and another data frame containing the tin
+#' numbers and their known masses. This step is left to the user; otherwise the
+#' tins can be manually weighed and included in the raw data before it is
+#' imported to R.
 #'
 #'@format A tibble with 6 rows and 4 variables:
+#'
 #'\describe{
-#'\item{tin_w_wet_sample}{mass of tin + mass of wet sample}
-#'\item{tin_w_OD_sample}{mass of tin + mass of oven-dry ("OD") sample}
-#'\item{tin_number}{the number assigned to the aluminum weighing tin}}
+#'   \item{tin_w_wet_sample}{mass of tin + mass of wet sample}
+#'   \item{tin_w_OD_sample}{mass of tin + mass of oven-dry ("OD") sample}
+#'   \item{tin_number}{the number assigned to the aluminum weighing tin}
+#'}
 #'
 "example_wcont"
